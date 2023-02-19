21841ce658e2409c19693da69466b96af931463e
digici19 <125779681+digici19@users.noreply.github.com>
2023-02-19 00:51:05 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function mostrar() {
+            div = document.getElementById('flotante');
+            div.style.display = '';
+        }
+        
+        function cerrar() {
+            div = document.getElementById('flotante');
+            div.style.display = 'none';
+        }
+
+function mostrar1() {
+            div = document.getElementById('flotante1');
+            div.style.display = '';
+        }
+        
+        function cerrar1() {
+            div = document.getElementById('flotante1');
+            div.style.display = 'none';
+        }
