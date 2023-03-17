e913732b22d4cbe8d3a461da5b8f6db625ca4be2
digici19 <125779681+digici19@users.noreply.github.com>
2023-03-17 00:09:36 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -17,3 +17,16 @@ function mostrar1() {
             div = document.getElementById('flotante1');
             div.style.display = 'none';
         }
+function solicitud(valor) {
+            var nombre2 = document.getElementById("nombre").value;
+            
+            var email2 = document.getElementById("email").value;
+                    
+            var solicitud2 = document.getElementById('solicitud1').value;
+			
+			if (nombre==""){
+			alert ("el nombre de la empresa debe ingresarse obligatoriamente")
+			
+			}
+            console.log(nombre2,email2,solicitud2);
+        } 
